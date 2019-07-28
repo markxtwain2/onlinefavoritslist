dc70854b1f15c718f68ea2b7bd571cb3c6bd13f3
Mahdi Naderian <naderian.mahdi@gmail.com>
2019-07-28 18:23:41 +0430

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+	include('includes/functions.php');
+
+?>
+<html>
+	<head>
+		<title>Login</title>
+		<link href="styles/login.css" type="text/css" rel="stylesheet" />
+		<link rel="icon" type="image/png" href="images/favorites.png" />
+	</head>
+	<body>
+		<div id="main">
+			<h3>Favorites List <span>v1.0</span></h3>
+			<?php echo $_SESSION['message']; unset($_SESSION['message']); ?>
+			<fieldset>
+				<legend>Login</legend>
+				<form id="login_form" method="post" action="login_check.php">
+					<label>Username: </label><input class="text" type="text" name="username" /><br />
+					<label>Password: </label><input class="text" type="password" name="password" /><br />
+					<input class="submit" type="submit" value="Login" name="login" />
+				</form>
+			</fieldset>
+			<a id="register_here" href="register.php" title="Register here, please!">Register here</a>
+		</div>
+	</body>
+</html>
